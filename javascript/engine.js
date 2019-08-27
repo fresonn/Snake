f7cb787c72f756ba0b7b133397a4cb9e55f0268f
Shevlyakov Vladislav <vladislavs319@gmail.com>
2019-08-27 14:59:28 +0300

    stable stage

--- javascript/engine.js
+++ javascript/engine.js
@@ -1,50 +1,130 @@
 /*
     Тут вся основная логика игры!
+    •Нужно как-то очищать предыдущий кадр, для отрисовки нового кадра анимации
 */
 
-const canvas = document.querySelector("#cnvs")
 
 class Game {
-
-    static nameOfGame = "SnakeJS"
     
-
     constructor() {
         this.canvas = document.querySelector("#cnvs")
         this.ctx = this.canvas.getContext("2d")
+        this.coin = null
         this.game = {
             cells: [],
             cellSize: 20
         }
         this.snake = {
             cells: [],
+            speed: 100, // в ms
+            isMoving: false,
+            currentRoute: null,
+            routes: {
+                left: {
+                    row: 0,
+                    col: -1,
+                },
+                up: {
+                    row: -1,
+                    col: 0
+                },
+                right: {
+                    row: 0,
+                    col: 1
+                },
+                down: {
+                    row: 1,
+                    col: 0
+                }
+            }
         }
         this.sprites = {
             body: null,
             cell: null,
+            coin: null,
         }
+        this.removeKeyHandler = this.keyHandler.bind(this)
     }
 
-    start() {
-       
+
+
+    build() {
         this.buildGrid()
         this.buildSnake()
+        this.buildCoin()
 
-        
         this.assetsPreload(() => {
             this.launch()
         })
+        this.snake.currentRoute = this.snake.routes.up
+    }
+
+
+
+    start() {
+    
+        this.build()
+        this.action()
+        this.snake.isMoving = true
+
+        this.timer = setInterval(() => {
+            this.move()
+            this.launch()
+        }, this.snake.speed);
 
     }
 
-    launch() {
+    /*
+        Поскольку методы ES6 эквивалентны обычным функциям, а это значит - они теряют свой контекст
+        тут отлично подойдет стрелочная функция, она получает контекст при определении, а не при вызове!!!
+        Но это только в Chrome(
+    */
+    keyHandler(event) {
+        const {
+            up,
+            down,
+            left,
+            right
+        } = this.snake.routes
 
+        switch (event.keyCode) {
+            case 87: // up
+                this.snake.currentRoute = up
+            break
+            case 68:
+                this.snake.currentRoute = right
+            break
+            case 83:
+                this.snake.currentRoute = down
+            break
+            case 65:
+                this.snake.currentRoute = left
+            break
+            default:
+                return
+        }
+    }
+
+
+
+    action() {
+        window.addEventListener("keydown", this.removeKeyHandler)
+    }
+
+
+
+    launch() {
         // вывод и параметры каждого изображения
         window.requestAnimationFrame(() => {
+            this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
             this.renderGrid()
             this.renderSnake()
         })
+
+
     }
+
+
 
     assetsPreload(funcAfterAll) {
 
@@ -59,7 +139,6 @@ class Game {
             }
         }
         
-
         for (let key in this.sprites) {
             this.sprites[key] = new Image
             this.sprites[key].src = `assets/${key}.png`
@@ -78,9 +157,9 @@ class Game {
             row,
             col
         }
-
         return cellObj
     }
+
 
 
     buildGrid() {
@@ -91,22 +170,30 @@ class Game {
                 this.game.cells.push(this.createCell(row, col))
             }
         }
-        console.log(this.game.cells)
     }
+
 
 
     renderGrid() {
+        // Все полученные координаты ячеек нужно заполнить
         for (let cell of this.game.cells) {
             this.ctx.drawImage(this.sprites.cell, cell.x, cell.y)
+
+            if (cell.food) {
+                this.ctx.drawImage(this.sprites.coin, cell.x, cell.y)
+            }
         }
+
+        
     }
+
+
 
     getGridCell(row, col) {
         return this.game.cells.find(cell => cell.row === row && cell.col === col);
     }
 
 
-    // 
 
     buildSnake() {
 
@@ -114,6 +201,7 @@ class Game {
             { row: 20, col: 20 }, // голова змеи
             { row: 21, col: 20 },
             { row: 22, col: 20 },
+            { row: 23, col: 20 },
         ]
 
         for (let snakeCell of startSnake) {
@@ -138,43 +226,69 @@ class Game {
 
     getNextCell() {
         const head = this.snake.cells[0]
-        const row = head.row - 1
-        const col = head.col 
 
-        let target = this.game.cells.find(cell => {
-            return cell.row === row && cell.col === col
-            
-        })
+        // Важно менять текущий маршрут для изменения движения
+        const { row , col } = this.snake.currentRoute
 
-        return target
+        const snakeRow = head.row + row
+        const snakeCol = head.col + col
+
+        // вернет ячейку
+        return this.game.cells.find(cell => cell.row === snakeRow && cell.col === snakeCol)
     }
     
     move() {
+
+        if (!this.snake.isMoving) return // ну тут ясно
+
+        console.log('move')
+
         const nextCell = this.getNextCell() // bool
 
         if (nextCell) {
             // +1 в начало
             this.snake.cells.unshift(nextCell) // каждая новая ячейка - голова
-            // -1 в конце
-            this.snake.cells.pop()
+            if (!this.isCoin(nextCell)) {
+                // -1 в конце
+                this.snake.cells.pop()
+            } else {
+                this.coin.food = false
+                this.buildCoin()
+            }
+        } else {
+            console.log('stop')
+            clearInterval(this.timer)
+            window.removeEventListener('keydown', this.removeKeyHandler)
         }
     }
 
-    x() {
-        // console.log(this.snake.cells)
-        this.move()
-        this.renderSnake()
-        // console.log(this.snake.cells)
+
+    isSnakeCells(findCell) {
+        this.game.cells.find(cell => {
+            return cell === findCell
+        })
+    }
+
+    isCoin(findCell) {
+        return findCell.food
+    }
+
+    // 
+    buildCoin() {
+        const getRandomCell = (m, n) => {
+            return m + Math.floor((n - m + 1) * Math.random())
+        }
+
+
+        const availableCells = this.game.cells.filter(findCell => !this.isSnakeCells(findCell))
+
+
+        const cell = this.game.cells[getRandomCell(20, availableCells.length - 1)]
+        cell.food = true
+        this.coin = cell
     }
 
 }
-
-for (let x = 0; x <= 10; x++) {
-	setTimeout(() => {
-		game.x()
-	}, x * 1000)
-}
-
 
 const game = new Game
 game.start()
